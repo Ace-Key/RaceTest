23852b343de9f1acec11f5e7ecf8af3019dc4ef3
Pita T.H <key.lockcard22@gmail.com>
2021-02-20 17:50:14 +1300

    Initial commit

---
+++ Assets/Scripts/ScoreKeeping.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreKeeping : MonoBehaviour
+{
+    [SerializeField] int score;
+    [SerializeField] TextMeshProUGUI scoreCard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreCard.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TestingOne
+ A Unity test with some of the meshes. A quick scene with some capsule cars and  a little bit of code.
